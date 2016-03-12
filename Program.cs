5ddb6fe5c914587b11da62cee0f8ab4b272139f8
gogear777 <495436536@qq.com>
2016-03-12 16:35:12 +0800

    sql connection
    建立数据库连接

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ConnectionTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connstring = @"Data Source=SC-201510032109\SQLEXPRESS;Initial Catalog=Students;Integrated Security=True;Pooling=False";
+            SqlConnection conn = new SqlConnection(connstring);
+            try
+            {
+                conn.Open();
+                Console.WriteLine("Successfully Connected");
+            }
+            catch(SqlException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conn.Close();
+                Console.WriteLine("Connection closed");
+            }
+        }
+    }
+}
